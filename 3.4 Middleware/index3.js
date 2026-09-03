5e37c7457dfce5676151aa535600b85df6a244ff
dheekshaa <dheekshanaveen12@gmail.com>
2026-09-04 01:11:43 +0530

    user defined middleware learnt and implemented

--- 3.4 Middleware/index3.js
+++ 3.4 Middleware/index3.js
@@ -1,7 +1,18 @@
 import express from "express";
-
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import bodyParser from "body-parser";
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+const logger = (req, res, next) => {
+  console.log(req.url);
+  console.log(req.method);
+  next();
+};
 
 app.use(logger);
 
--- 3.5 Secrets Project/solution.js
+++ 3.5 Secrets Project/solution.js
@@ -2,29 +2,36 @@ import express from "express";
 import bodyParser from "body-parser";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
-
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
 const app = express();
 const port = 3000;
-let bandname = "";
-function bandNameGenerator(req, res, next) {
-  console.log(req.body);
-  bandname=req.body["street"] + req.body["pet"];
-  next();
-};
 
+var userIsAuthorised = false;
 
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bandNameGenerator);
+
+function passwordCheck(req, res, next) {
+  const password = req.body["password"];
+  if (password === "ILoveProgramming") {
+    userIsAuthorised = true;
+  }
+  next();
+}
+app.use(passwordCheck);
+
 app.get("/", (req, res) => {
   res.sendFile(__dirname + "/public/index.html");
 });
-app.post("/submit", (req, res) => {
-  res.send(`YOUR BANDNAME IS: ${bandname}`);
-  console.log(req.body);
-});
 
+app.post("/check", (req, res) => {
+  if (userIsAuthorised) {
+    res.sendFile(__dirname + "/public/secret.html");
+  } else {
+    res.sendFile(__dirname + "/public/index.html");
+    //Alternatively res.redirect("/");
+  }
+});
 
 app.listen(port, () => {
   console.log(`Listening on port ${port}`);
