785599618eaef0bbb10849acff978a178ccde741
dheekshaa <dheekshanaveen12@gmail.com>
2026-09-10 13:26:39 +0530

    ejs learnt and implemented, it lets u if its weekend or weekday and gives advice accordingly.

--- 4.0 EJS/index.js
+++ 4.0 EJS/index.js
@@ -1,5 +1,21 @@
-var dateTime = require('get-date');
- 
-console.log(dateTime());
- 
-console.log(dateTime(true));
+import express from "express";
+
+const app = express();
+const port = 3000;
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  const today = new Date("2026-09-12T00:00:00");
+  const day = today.getDay();
+  let type = "Weekday";
+  let advice = "Time to work!";
+  if (day === 0 || day === 6) {
+    type = "Weekend";
+    advice = "Time to relax!";
+  }
+  res.render("index.ejs", { dayType: type, advice: advice });
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
